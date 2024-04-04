6b7b55d635dc87647248ebd34ab7d4f20ba6ee1f
tungisdab <khanhdzno1st01061102@gmail.com>
2024-04-04 22:43:36 +0700

    update

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,13 +1,10 @@
-a = input()
-b = input()
-az = 0
-bz = 0
-n = len(a)
-for i in range(n):
-    if a[i] == '1':
-        az += 2**(n-i-1)
-m = len(b)
+n = int(input())
+n = str(n)
+print(n)
+m = len(n)
+x = ''.join(reversed(n))
+print(x)
+a = 0
 for i in range(m):
-    if b[i] == '1':
-        bz += 2**(m-i-1)
-c = az + bz
+    a += (int(x[i]) * 2**(m-i-1))
+print(a)
