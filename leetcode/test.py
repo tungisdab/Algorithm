abb632134fe3828eda724498f1e6e3c193941f0c
tungisdab <khanhdzno1st01061102@gmail.com>
2024-04-16 12:40:50 +0700

    'update

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,33 +1,36 @@
-words = ["Hello","Alaska","Dad","Peace"]
-a = [ 'q','w','e','r','t','y','u','i','o','p', ]
-b = [ 'a','s','d','f','g','h','j','k','l', ]
-c = [ 'z','x','c','v','b','n','m', ]
-ans = []
-m = len(words)
-for i in range(m):
-    n = len(words[i])
-    cc = 0
-    if words[i][0].lower() in a:
-        cc = 1
-    if words[i][0] in b:
-        cc = 2
-    if words[i][0] in c:
-        cc = 3
-    print(words[i][0].lower())
-    print(cc)
-    k = 1
-    for j in range(1, n):
-        if cc == 1 and words[i][j] not in a:
-            k = 0
-            break
-        if cc == 2 and words[i][j] not in b:
-            k = 0
-            break
-        if cc == 3 and words[i][j] not in c:
-            k = 0
-            break
-    if k == 1:
-        ans.append(words[i]) 
-print(*ans)
-            
+# words = ["Hello","Alaska","Dad","Peace"]
+# a = [ 'q','w','e','r','t','y','u','i','o','p', ]
+# b = [ 'a','s','d','f','g','h','j','k','l', ]
+# c = [ 'z','x','c','v','b','n','m', ]
+# ans = []
+# m = len(words)
+# for i in range(m):
+#     n = len(words[i])
+#     cc = 0
+#     if words[i][0].lower() in a:
+#         cc = 1
+#     if words[i][0] in b:
+#         cc = 2
+#     if words[i][0] in c:
+#         cc = 3
+#     k = 1
+#     for j in range(1, n):
+#         if cc == 1 and words[i][j] not in a:
+#             k = 0
+#             break
+#         if cc == 2 and words[i][j] not in b:
+#             k = 0
+#             break
+#         if cc == 3 and words[i][j] not in c:
+#             k = 0
+#             break
+#     if k == 1:
+#         ans.append(words[i]) 
+# print(*ans)
+# print('hehe')            
+# print('kkKey Bindings')
 
+s = ""
+a = [str(i) for i in s.split(":")]
+print(*a)
+print(len(a))
--- leetcode/testz.cpp
+++ leetcode/testz.cpp
@@ -5,27 +5,14 @@ typedef double db;
 //by KMA dil and man
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+
     int n;
     cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    int k;
-    cin>>k;
-    int l = 0, r = n-1;
-    int m = -1;
-    while(l<=r){
-        m = (l+r)/2;
-        if (a[m] == k) 
-            break;
-        else if(a[m] < k)
-            l = m+1;
-        else
-            r = m-1;
-    }
-    cout<<m<<endl;
+    double a = log(double(n));
+    cout<<a;
+    if ((int) a == a) 
+        cout<<'1';
+    else 
+        cout<<'0';
     return 0;
 }
