497f4c3b483fb817adb893b4402c92792e5af9bc
tungisdab <khanhdzno1st01061102@gmail.com>
2024-04-18 16:10:27 +0700

    update

--- leetcode/testz.cpp
+++ leetcode/testz.cpp
@@ -5,14 +5,40 @@ typedef double db;
 //by KMA dil and man
 int main()
 {
-
-    int n;
-    cin >> n;
-    double a = log(double(n));
-    cout<<a;
-    if ((int) a == a) 
-        cout<<'1';
-    else 
-        cout<<'0';
-    return 0;
+    // vector<vector<int>> grid = {{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}};
+    vector<vector<int>> grid = {{0,1}};
+    for(auto i: grid) {
+        for (auto j: i)
+            cout<< j<< " ";
+        cout<<endl;
+    }
+    int n = grid.size();
+    vector<vector<int>> a(n+2, vector<int> (n+2, 0));
+    for(int i =0;i<n;i++) {
+        for (int j =0;j<n;j++){
+            a[i+1][j+1] = grid[i][j];
+        }
+    }
+    for (auto i: a) {
+        for (int j : i) {
+            cout << j << " ";
+        }
+        cout<<endl;
+    }
+    int ans = 0;
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n;j++) {
+            if (a[i][j] == 1) {
+                ans += 4;
+                if (a[i-1][j] == 1)
+                    ans--;
+                if (a[i+1][j] == 1)
+                    ans--;
+                if (a[i][j-1] == 1)
+                    ans--;
+                if (a[i][j+1] == 1)
+                    ans--;
+            }
+        }
+    }
 }
