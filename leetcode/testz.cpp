41a6f46ab78d4bcaedc10ee1d65779a63728a0ae
tungisdab <khanhdzno1st01061102@gmail.com>
2024-04-21 15:12:54 +0700

    update

--- leetcode/testz.cpp
+++ leetcode/testz.cpp
@@ -5,40 +5,13 @@ typedef double db;
 //by KMA dil and man
 int main()
 {
-    // vector<vector<int>> grid = {{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}};
-    vector<vector<int>> grid = {{0,1}};
-    for(auto i: grid) {
-        for (auto j: i)
-            cout<< j<< " ";
-        cout<<endl;
-    }
-    int n = grid.size();
-    vector<vector<int>> a(n+2, vector<int> (n+2, 0));
-    for(int i =0;i<n;i++) {
-        for (int j =0;j<n;j++){
-            a[i+1][j+1] = grid[i][j];
-        }
-    }
-    for (auto i: a) {
-        for (int j : i) {
-            cout << j << " ";
-        }
-        cout<<endl;
-    }
-    int ans = 0;
-    for (int i=1;i<=n;i++){
-        for (int j=1;j<=n;j++) {
-            if (a[i][j] == 1) {
-                ans += 4;
-                if (a[i-1][j] == 1)
-                    ans--;
-                if (a[i+1][j] == 1)
-                    ans--;
-                if (a[i][j-1] == 1)
-                    ans--;
-                if (a[i][j+1] == 1)
-                    ans--;
-            }
-        }
-    }
+    int n;
+    cin>>n;
+    double a = log(n) / log(3);
+    // cout<<log(243) / log(3)<<endl;
+    cout<< a << endl;
+    cout<<abs(a-int(a))<<endl;
+    // if ( abs(a-int(a)) < 0.1   )
+    //     cout<< "true";
+    // else cout<< "false";
 }
