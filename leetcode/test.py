b4ed5a6a70794db3ff752b3ced64442a922a9320
tungisdab <khanhdzno1st01061102@gmail.com>
2024-05-05 11:03:35 +0700

    i[d

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,3 +1,2 @@
-nums = [0,2,1,5,3,4]
-a = [nums[i] for i in nums]
-print(a)
+a = 'a'
+print(int(a))
--- leetcode/testz.cpp
+++ leetcode/testz.cpp
@@ -5,13 +5,6 @@ typedef double db;
 //by KMA dil and man
 int main()
 {
-    int n;
-    cin>>n;
-    double a = log(n) / log(3);
-    // cout<<log(243) / log(3)<<endl;
-    cout<< a << endl;
-    cout<<abs(a-int(a))<<endl;
-    // if ( abs(a-int(a)) < 0.1   )
-    //     cout<< "true";
-    // else cout<< "false";
+    char a = 'a';
+    cout<<int(a)<<endl;
 }
