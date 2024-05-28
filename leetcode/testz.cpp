a2bea937fc9387264f8f2262442714b151fcd1a0
tungisdab <khanhdzno1st01061102@gmail.com>
2024-05-28 11:27:11 +0700

    update

--- leetcode/testz.cpp
+++ leetcode/testz.cpp
@@ -3,30 +3,22 @@ using namespace std;
 typedef long long ll;
 typedef double db;
 //by KMA dil and man
+
+static map<char, int> mp;
+static bool cmp(char a, char b) {
+    return mp[a] > mp[b];
+}
+string frequencySort(string s) {
+    for (char i:s) {
+        mp[i]++;
+    }
+    sort(s.begin(), s.end(), cmp);
+    return s;
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n;
-    cin>>n;
-    vector<vector<int>> a(n,vector<int> (n, 0));
-    int x = 1;
-    for(int i=0;i<n/2;i++) {
-        for (int j=i;j<n-i;j++) 
-            a[i][j] = x++;
-        for (int j=i;j<n-i;j++)
-            a[j][n-1-i] = x++;
-        for (int j=i;j<n-i;j++)
-            a[n-1-i][n-1-j] = x++;
-        for (int j=i;j<n-i;j++)
-            a[n-1-j][i] = x++;
-    }
-    for (int i=0;i<n;i++) {
-        for (int j=0;j<n;j++)
-            cout<<a[i][j]<<" ";
-        cout<<endl;
-    }
+    string s = "tree";
+    cout << frequencySort(s) << endl;
     return 0;
 }
