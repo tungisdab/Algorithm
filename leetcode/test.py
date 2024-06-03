0ddeaafc5990b7b075eff04c06803a51a5c5a207
tungisdab <khanhdzno1st01061102@gmail.com>
2024-06-03 07:41:09 +0700

    03.06

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,18 +1,7 @@
-start = 3
-goal = 4
-a = bin(start)
-b = bin(goal)
-a = a.lstrip('-0b')
-b = b.lstrip('-0b')
-print(a, b)
-while len(a) < len(b):
-    a = '0' + b
-while len(a) > len(b):
-    b = '0' + b
-print(a, b)
-n = len(a)
-ans = 0
-for i in range(n):
-    if a[i] != b[i]:
-        ans += 1
-print(ans)
+words = ["abc","car","ada","racecar","cool"]
+for i in words:
+    x = ''.join(reversed(i))
+    print(i)
+    print(x)
+    if x == i:
+        print(i) 
