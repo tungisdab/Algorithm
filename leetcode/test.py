ca50bc89a1edf91542d179b8b4654304275483d5
tungisdab <khanhdzno1st01061102@gmail.com>
2024-06-12 09:19:07 +0700

    12.06

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,16 +1,16 @@
-m = 2
-n = 3
-indices = [[0,1],[1,1]]
-a = [[0] * n] * m
-for i, j in indices:
-    for x in range(n):
-        a[i][x] = a[i][x] + 1
-    for x in range(m):
-        a[x][j] = a[x][j] + 1
-print(*a)
+strs = ["abc", "bce", "cae"]
+n = len(strs)
+m = len(strs[0])
+a = []
+for i in range(m):
+    x = []
+    for j in range(n):
+        x.append(strs[j][i])
+    a.append(x)
 ans = 0
 for i in a:
-    for j in i:
-        if j%2:
-            ans += 1
-print(ans)
+    x = i
+    x.sort()
+    print(i, x)
+    if x != i:
+        ans += 1
