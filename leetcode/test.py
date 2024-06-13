9dfa18540cdfd800cdb98bd72ae190a7ef0d7126
tungisdab <khanhdzno1st01061102@gmail.com>
2024-06-13 08:13:08 +0700

    13.06

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,16 +1,21 @@
-strs = ["abc", "bce", "cae"]
-n = len(strs)
-m = len(strs[0])
-a = []
-for i in range(m):
-    x = []
-    for j in range(n):
-        x.append(strs[j][i])
-    a.append(x)
-ans = 0
-for i in a:
-    x = i
-    x.sort()
-    print(i, x)
-    if x != i:
-        ans += 1
+s = "erase*****"
+a = list(s)
+n = len(s)
+i = 0
+while i < n:
+    if a[i] == "*":
+        j = i -1
+        while j >= 0:
+            if a[j] != "*":
+                del a[j]
+                n -= 1
+                i -= 1
+                break
+            else:
+                j -= 1
+        del a[i]
+        n -= 1
+        i -= 1
+    i += 1
+    print(''.join(a))
+print(''.join(a))
