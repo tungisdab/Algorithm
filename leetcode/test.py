903c54ecdba90caab6c4dba4a14320ea088b6342
tungisdab <khanhdzno1st01061102@gmail.com>
2024-06-14 07:22:27 +0700

    14.06

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,21 +1,8 @@
-s = "erase*****"
-a = list(s)
+s = "xyzzaz"
+cnt = 0
 n = len(s)
-i = 0
-while i < n:
-    if a[i] == "*":
-        j = i -1
-        while j >= 0:
-            if a[j] != "*":
-                del a[j]
-                n -= 1
-                i -= 1
-                break
-            else:
-                j -= 1
-        del a[i]
-        n -= 1
-        i -= 1
-    i += 1
-    print(''.join(a))
-print(''.join(a))
+for i in range(0, n-2):
+    if len(s[i:i+3]) == len(set(s[i:i+3])):
+        cnt += 1
+        print(s[i:i+2])
+print(cnt)
