289bc4004e42e52675157d707212d140b2599eaf
tungisdab <khanhdzno1st01061102@gmail.com>
2024-06-16 08:10:00 +0700

    16.06

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,8 +1,17 @@
-# arr = [1,3,4,8]
-# ans = []
-# a = [0, arr[0]]
-# n = len(arr)
-# for i in range(1, n):
-#     a.append(arr[i] ^ a[i])
-# print(*a)
-print(1 ^ 1)
+strs = ["alic3","bob","3","4","00000"]
+ans = 0
+for i in strs:
+    x, y = 0, 0
+    for j in i:
+        print(j)
+        if j.isalpha():
+            x = 1
+        elif j.isdigit():
+            y = 0
+    
+    if x == 1 and y == 1:
+        ans = max(ans, len(i))
+    elif x == 0 and y == 1:
+        ans = max(ans, int(i))
+print(ans)
+# return ans
