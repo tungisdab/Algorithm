d6fb76519ad14f2e0f6e10218f921ec970156916
tungisdab <khanhdzno1st01061102@gmail.com>
2024-06-23 08:59:38 +0700

    23.06

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,7 +1,3 @@
-s1 = "apple apple"
-s2 = "banana"
-s1 = [i for i in s1.split()]
-s2 = [i for i in s2.split()]
-s1 = set(s1)
-s2 = set(s2)
-print((s1 - s2) | (s2 - s1))
+for i in range(5, 30):
+    x = "\""  + str(i) + 'th' + "\""
+    print(x, end = ', ')
