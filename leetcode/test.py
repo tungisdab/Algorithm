8c0bd3e882359d6ef44cad03251f7a74951296e1
tungisdab <khanhdzno1st01061102@gmail.com>
2024-06-25 22:45:58 +0700

    25.06

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,6 +1,14 @@
-from collections import Counter
+sentence = "a a ba"
+a = [i for i in sentence.split()]
+n = len(a)
+x = a[0][-1]
+for i in range(1, n):
+    if x != a[i][0]:
+        print(x, a[i][0])
+        break
+    print(x, a[i][0])
+    x = a[i][-1]
 
+if a[n-1][-1] != a[0][0]:
+    print("kasf")
 
-s = 'aaaaabbbcccddd'
-x = Counter(s)
-print(len(x))
