12105bfd7f65981257ba10054b0134d4437823df
tungisdab <khanhdzno1st01061102@gmail.com>
2024-06-30 16:52:02 +0700

    30.06

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,18 +1,3 @@
-
-def check(n):
-    if n < 2:
-        return False
-    if n == 2 or n == 3:
-        return True
-    if n % 2 == 0:
-        return False
-    for i in range(3, int(n**0.5) + 1, 2):
-        if n % i:
-            return False
-    return True
-
-for i in range(990, 1049):
-    x = bin(i)
-    k = x.count('1')
-    if check(k):
-        print(k, i)
+a = "gadsfdsaf"
+x = sorted(a)
+print(x)
