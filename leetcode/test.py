43e090807d488c62edd4b473baa3b319f532b1dd
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-05 16:59:50 +0700

    05.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,17 +1,1 @@
-s = "lEetcOde"
-x = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
-a = list(s)
-b = [i for i in a if i in x]
-print(b)
-n = len(b)
-for i in range(n-1):
-    for j in range(i+1, n):
-        if b[i] > b[j]:
-            b[i], b[j] = b[j], b[i]
-print(b)
-z = 0
-for i in range(len(a)):
-    if i in x:
-        a[i] = b[z]
-        z += 1
-print(''.join(a))
+print(1^3)
