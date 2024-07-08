8fe9670a60a0ead9aad7f300876d691df51df25d
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-08 11:29:07 +0700

    08.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,1 +1,13 @@
-print(1^3)
+operations = ["5","-2","4","C","D","9","+","+"]
+ans = []
+for i in operations:
+    if i.isdigit():
+        ans.append(int(i))
+    if i == "C":
+        ans.pop()
+    if i == "D":
+        ans.append(ans[-1] * 2)
+    if i == "+":
+        ans.append(ans[-1] + ans[-2])
+    print(ans)
+print(sum(ans))
