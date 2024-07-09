142384f7450fd645331b462a3edf791c126387ec
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-09 16:53:35 +0700

    09.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,13 +1,18 @@
-operations = ["5","-2","4","C","D","9","+","+"]
-ans = []
-for i in operations:
-    if i.isdigit():
-        ans.append(int(i))
-    if i == "C":
-        ans.pop()
-    if i == "D":
-        ans.append(ans[-1] * 2)
-    if i == "+":
-        ans.append(ans[-1] + ans[-2])
-    print(ans)
-print(sum(ans))
+grid = [[2,0,0,1],[0,3,1,0],[0,5,2,0],[4,0,0,2]]
+n = len(grid)
+for i in range(n):
+    for j in range(n):
+        if j == i:
+            if grid[i][j] == 0:
+                # return False
+                print(1)
+        if j == n - 1 - i:
+            if grid[i][j] == 0:
+                # return False
+                print(2)
+        else:
+            if grid[i][j] != 0:
+                print(i, j)
+                # return False
+                print(3)
+        # return True
