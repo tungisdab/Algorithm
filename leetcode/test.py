3be04a3425e4a01cfab8e1dbc58b9087155e3f0a
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-10 11:46:15 +0700

    10.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,18 +1,3 @@
-grid = [[2,0,0,1],[0,3,1,0],[0,5,2,0],[4,0,0,2]]
-n = len(grid)
-for i in range(n):
-    for j in range(n):
-        if j == i:
-            if grid[i][j] == 0:
-                # return False
-                print(1)
-        if j == n - 1 - i:
-            if grid[i][j] == 0:
-                # return False
-                print(2)
-        else:
-            if grid[i][j] != 0:
-                print(i, j)
-                # return False
-                print(3)
-        # return True
+# x = ['a', 'e', 'i', 'o', 'u']
+x = [1, 2, 3, 4, 5]
+print('.'.join(map(str, x)))
