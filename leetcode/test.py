10f47359bb9d6a2f33ce0624434012b44ac4e310
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-13 18:19:08 +0700

    13.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,2 +1,27 @@
-a = []
-print(len(a))
+from collections import Counter
+
+
+s = "ccccndxxlzerbsrrkvdnlvynxbjtjldsqgevphdlrldyishznryttvuratvwiafiwyjklafesvmcexuacxqgmnokfljxkystcbef"
+target = "bvciovnpto"
+x = Counter(s)
+y = Counter(target)
+m = float('inf')
+z = set(target)
+if len(z) == 1:
+    i = target[0]
+    if x[i] < y[i]:
+        # return 0
+        print(0)
+    else:
+        # return x[i] // y[i]
+        print(x[i] // y[i])
+for i in y:
+    if x[i] == 0:
+        print(0)
+    else:
+        if x[i] < y[i]:
+            m = min(m, x[i])
+        else:
+            m = min(m, x[i] // y[i])
+    print(m, i, x[i], y[i])
+# return m
