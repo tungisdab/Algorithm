56363825e5f511b67f2495f3dbc18b82b1adea75
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-18 19:13:51 +0700

    18.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,16 +1,10 @@
-nums = [847,847,0,0,0,399,416,416,879,879,206,206,206,272]
-n = len(nums)
+arr = [1,0,2,3,0,4,5,0]
+n = len(arr)
 i = 0
-while i < n - 1:
-    if nums[i] == nums[i+1]:
-        nums[i] *= 2
-        nums[i + 1] = 0
+while i < n:
+    if arr[i] == 0:
+        arr.insert(i, 0)
+        arr.pop
+        i += 1
     i += 1
-print(*nums)
-ans = [0] * n
-x = 0
-for i in range(n):
-    if nums[i]!= 0:
-        ans[x] = nums[i]
-        x += 1
-print(*ans)
+    print(arr)
