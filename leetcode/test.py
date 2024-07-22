ce82b3125142b1f2a7c33d545cdb007a881b28a4
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-23 00:12:20 +0700

    22.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,10 +1,25 @@
-arr = [1,0,2,3,0,4,5,0]
-n = len(arr)
-i = 0
-while i < n:
-    if arr[i] == 0:
-        arr.insert(i, 0)
-        arr.pop
-        i += 1
-    i += 1
-    print(arr)
+s = "a0b1c2"
+ans = ""
+a = []
+b = []
+for i in s:
+    if i.isdigit():
+        a.append(i)
+    else:
+        b.append(i)
+if abs(len(a) - len(b)) > 1:
+    # return ''
+    print('xxxxxxxxxxx')
+print(a)
+print(b)
+if len(a) >= len(b):
+    while len(a) == 0 and len(b) == 0:
+        ans += a.pop()
+        if b:
+            ans += b.pop()
+else:
+    while len(a) == 0 and len(b) == 0:
+        ans += b.pop()
+        if a:
+            ans += a.pop()
+print(ans)
