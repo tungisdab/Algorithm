1418b713d0023cfc48178728f1d3f002a5deca46
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-23 10:57:50 +0700

    23.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,25 +1,10 @@
-s = "a0b1c2"
-ans = ""
-a = []
-b = []
-for i in s:
-    if i.isdigit():
-        a.append(i)
-    else:
-        b.append(i)
-if abs(len(a) - len(b)) > 1:
-    # return ''
-    print('xxxxxxxxxxx')
-print(a)
-print(b)
-if len(a) >= len(b):
-    while len(a) == 0 and len(b) == 0:
-        ans += a.pop()
-        if b:
-            ans += b.pop()
-else:
-    while len(a) == 0 and len(b) == 0:
-        ans += b.pop()
-        if a:
-            ans += a.pop()
-print(ans)
+s = "abcd"
+x = list(s)
+y = x[::-1]
+n = len(x)
+for i in range(n):
+    if x[i] != y[i]:
+        x[i] = min(x[i], y[i])
+        y[i] = min(x[i], y[i])
+print(x)
+print(y)
