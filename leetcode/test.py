a702425885c4111bf2fd868ce1148d30a03829b0
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-24 19:48:11 +0700

    24.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,10 +1,15 @@
-s = "abcd"
-x = list(s)
-y = x[::-1]
-n = len(x)
-for i in range(n):
-    if x[i] != y[i]:
-        x[i] = min(x[i], y[i])
-        y[i] = min(x[i], y[i])
-print(x)
-print(y)
+code = [2,4,9,3]
+k = -2
+n = len(code)
+# if k == 0:
+#     return [0] * n
+a = code + code
+ans = []
+if k < 0:
+    for i in range(n, 2 * n):
+        ans.append(sum(a[i - k - 1:i]))
+    print(ans)
+else:
+    for i in range(0, n):
+        ans.append(sum(a[i + 1:i+k+1]))
+# return ans
