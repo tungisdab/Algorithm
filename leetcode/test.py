4deb02b964e5f84e7fcfbf079bc0cd7f73a28f2e
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-27 15:50:48 +0700

    27.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,15 +1,11 @@
-code = [2,4,9,3]
-k = -2
-n = len(code)
-# if k == 0:
-#     return [0] * n
-a = code + code
-ans = []
-if k < 0:
-    for i in range(n, 2 * n):
-        ans.append(sum(a[i - k - 1:i]))
-    print(ans)
-else:
-    for i in range(0, n):
-        ans.append(sum(a[i + 1:i+k+1]))
-# return ans
+# s = "Bob hit a ball, the hit BALL flew far after it was hit."
+s = "a, a, a, a, b,b,b,c, c"
+s = s.lower()
+x = ''
+for i in s:
+    if i.isalpha() or i == ' ':
+        x += i
+    else: 
+        x +=' '
+x = list(x.split())
+print(x)
