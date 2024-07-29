a79f06d2528133dac84723a145bc0c368a7130c0
tungisdab <khanhdzno1st01061102@gmail.com>
2024-07-29 16:27:59 +0700

    29.07

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,10 +1,17 @@
-s = "ab"
-n = len(s)
-# if n < 2:
-#     return False
-for i in range(1, n // 2 + 2):
-    if n % i == 0:
-        x = s[:i]
-        a = x * (n // i)
-        if a == s:
-            print(a, s)
+a = [2,5,3,4,1]
+ans = 0
+n = len(a)
+for i in range(n, n-2):
+    for j in range(i+1, n-1):
+        print(a[i], a[j])
+
+        if a[i] < a[j]:
+            for k in range(j+1, n):
+                if a[j] < a[k]:
+                    ans += 1
+                    print(a[i], a[j], a[k])
+        elif a[i] > a[j]:
+            for k in range(j+1, n):
+                if a[j] > a[k]:
+                    ans += 1
+                    print(a[i], a[j], a[k])
