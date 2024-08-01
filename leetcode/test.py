d46356df8b0d5cc6ab570d1502d7bc8c4414cdbf
tungisdab <khanhdzno1st01061102@gmail.com>
2024-08-01 15:53:36 +0700

    01.08

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,8 +1,13 @@
-event1 = ["01:15","02:00"]
-event2 = ["02:00","03:00"]
-a, b = map(int,event1[0].split(':'))
-c, d = map(int,event1[1].split(':'))
-m, n = map(int,event2[0].split(':'))
-p, q = map(int,event2[1].split(':'))
-print(a, b, c, d)
-print(m, n, p, q)
+pattern = "acdcbbb"
+n = len(set(pattern))
+k = 0
+cc = ''
+x = []
+for i in pattern:
+    if i not in x:
+        cc = cc + str(k)
+        x.append(i)
+        k += 1
+    else:
+        cc = cc + str(x.index(i))
+print(cc)
