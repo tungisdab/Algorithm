a6670f48d1501092ea40b1637f6b40f48d109711
tungisdab <khanhdzno1st01061102@gmail.com>
2024-08-02 22:41:29 +0700

    02.08

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,13 +1,13 @@
-pattern = "acdcbbb"
-n = len(set(pattern))
-k = 0
-cc = ''
-x = []
-for i in pattern:
-    if i not in x:
-        cc = cc + str(k)
-        x.append(i)
-        k += 1
-    else:
-        cc = cc + str(x.index(i))
-print(cc)
+num1 = "1+-1i"
+num2 = "0+0i"
+num1 = num1.replace('i', '')
+num2 = num2.replace('i', '')
+a, b = num1.split('+')
+c, d = num1.split('+')
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+print(a, b, c, d)
+ans = str(a * c - b * d) + '+' + str(a * d + b * c) + 'i'
+print(ans)
