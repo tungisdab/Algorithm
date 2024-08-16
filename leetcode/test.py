c723528f1c46d117f736e037a81f99effb53ea9f
tungisdab <khanhdzno1st01061102@gmail.com>
2024-08-16 10:48:53 +0700

    16.08

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,11 +1,13 @@
-s = "abacaba"
-ans = 0
-x = set()
-for i in s:
-    if i not in x:
-        x.add(i)
-    else:
-        x.clear()
-        ans += 1
-    print(x)
-    print(ans)
+grid = [[1,0],[0,2]]
+x, y, z = 0, 0, 0
+for i in grid:
+    for j in i:
+        x += 1
+    y += max(i)
+n = len(grid)
+for i in range(n):
+    tmp = 0
+    for j in range(n):
+        tmp = max(tmp, grid[j][i])
+    z += tmp
+print(x, y, z)
