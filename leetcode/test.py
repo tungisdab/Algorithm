f867b7e59be7da81cac0f2218f5652a931fdc55a
tungisdab <khanhdzno1st01061102@gmail.com>
2024-08-25 10:50:21 +0700

    25.08

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,3 +1,14 @@
-rec1 = [0,0,2,2]
-a, b, c, d = rec1
-print(a, b, c, d)
+n = 5
+x = bin(n)
+x = x.lstrip('-0b')
+print(x)
+print(type(x))
+a = []
+for i in x:
+    if x == '0':
+        a.append('1')
+    else:
+        a.append('0')
+print(a)
+# return int(''.join(a), 2)
+# print(int(''.join(a), 2))
