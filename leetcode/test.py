152e5360defbe97cda2745a044ec5ea08fa5bdd7
tungisdab <khanhdzno1st01061102@gmail.com>
2024-09-02 10:46:45 +0700

    02.09

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,9 +1,10 @@
-n = 5
-ans = 0
-for i in range(1, n-1):
-    for j in range(i+1, n):
-        x = (i*i + j*j) ** 0.5
-        print(i, j, x)
-        if x == int(x) and x <= n:
-            ans += 1
-print(ans)
+a = [3,4,1,2]
+k = 25
+n = len(a)
+i = 0
+while a[i] > k:
+    k -= a[i]
+    i += 1
+    if i == n:
+        i = 0
+    print(i, k)
