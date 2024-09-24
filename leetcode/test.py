e06d804fdc48d60eba760f97b4e0012a8d4f8b52
tungisdab <khanhdzno1st01061102@gmail.com>
2024-09-24 10:18:25 +0700

    24.09

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,14 +1,12 @@
-output=[] 
-people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]        
-# sort the array in decreasing order of height 
-# within the same height group, you would sort it in increasing order of k
-# eg: Input : [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
-# after sorting: [[7,0],[7,1],[6,1],[5,0],[5,2],[4,4]]
-people.sort(key=lambda x: (-x[0], x[1]))    
-print(people)            
-for a in people:
-    # Now let's start the greedy here
-    # We insert the entry in the output array based on the k value
-    # k will act as a position within the array
-    output.insert(a[1], a)
-    print(output)
+def thn(n,a,b,c):
+    if n==1:
+        print(a," -> ", c)
+        return
+    thn(n-1,a,c,b)
+    thn(1,a,b,c)
+    thn(n-1,b,a,c)
+a='A'
+b='B'
+c='C'
+n=int(input())
+thn(n,a,b,c)
