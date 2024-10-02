63113c15d74d281e3b82263dfb0f9c456b1149ed
tungisdab <khanhdzno1st01061102@gmail.com>
2024-10-02 11:03:37 +0700

    02.10

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,12 +1,38 @@
-def thn(n,a,b,c):
-    if n==1:
-        print(a," -> ", c)
-        return
-    thn(n-1,a,c,b)
-    thn(1,a,b,c)
-    thn(n-1,b,a,c)
-a='A'
-b='B'
-c='C'
-n=int(input())
-thn(n,a,b,c)
+# tokens = ["2","1","+","3","*"]
+tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+i = 2
+for j in range(len(tokens)):
+    if tokens[j] != '+' and tokens[j] != '-' and tokens[j] != '*' and tokens[j] != '/':
+        tokens[j] = int(tokens[j])
+while len(tokens) != 1:
+    if tokens[i] == '+':
+        x = tokens[i-1] + tokens[i-2]
+        i -= 2
+        tokens.pop(i)
+        tokens.pop(i)
+        tokens.pop(i)
+        tokens.insert(i, x)
+    elif tokens[i] == '-':
+        x = tokens[i-2] - tokens[i-1]
+        i -= 2
+        tokens.pop(i)
+        tokens.pop(i)
+        tokens.pop(i)
+        tokens.insert(i, x)
+    elif tokens[i] == '*':
+        x = tokens[i-1] * tokens[i-2]
+        i -= 2
+        tokens.pop(i)
+        tokens.pop(i)
+        tokens.pop(i)
+        tokens.insert(i, x)
+    elif tokens[i] == '/':
+        x = tokens[i-2] // tokens[i-1]
+        i -= 2
+        tokens.pop(i)
+        tokens.pop(i)
+        tokens.pop(i)
+        tokens.insert(i, x)
+    i += 1
+    print(tokens)
+print(tokens[0])
