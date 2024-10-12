036c35110fda11b2afbf11cb86bc39e574c80709
tungisdab <khanhdzno1st01061102@gmail.com>
2024-10-12 11:17:24 +0700

    12/10

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,38 +1,8 @@
-# tokens = ["2","1","+","3","*"]
-tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
-i = 2
-for j in range(len(tokens)):
-    if tokens[j] != '+' and tokens[j] != '-' and tokens[j] != '*' and tokens[j] != '/':
-        tokens[j] = int(tokens[j])
-while len(tokens) != 1:
-    if tokens[i] == '+':
-        x = tokens[i-1] + tokens[i-2]
-        i -= 2
-        tokens.pop(i)
-        tokens.pop(i)
-        tokens.pop(i)
-        tokens.insert(i, x)
-    elif tokens[i] == '-':
-        x = tokens[i-2] - tokens[i-1]
-        i -= 2
-        tokens.pop(i)
-        tokens.pop(i)
-        tokens.pop(i)
-        tokens.insert(i, x)
-    elif tokens[i] == '*':
-        x = tokens[i-1] * tokens[i-2]
-        i -= 2
-        tokens.pop(i)
-        tokens.pop(i)
-        tokens.pop(i)
-        tokens.insert(i, x)
-    elif tokens[i] == '/':
-        x = tokens[i-2] // tokens[i-1]
-        i -= 2
-        tokens.pop(i)
-        tokens.pop(i)
-        tokens.pop(i)
-        tokens.insert(i, x)
-    i += 1
-    print(tokens)
-print(tokens[0])
+nums = [2,3,1,1,4]
+n = len(nums)
+i = 0
+while i + nums[i] < n:
+    m = max(nums[i:i+nums[i]+1])
+    z = nums[i:i+nums[i]+1:-1]
+    i += z.index(m)
+    print(z)
