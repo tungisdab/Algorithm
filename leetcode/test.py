a31ed0a80d8a4aad5919250542bb8bbd437da4b0
tungisdab <khanhdzno1st01061102@gmail.com>
2024-12-18 14:57:04 +0700

    18/12/2024

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,8 +1,2 @@
-nums = [2,3,1,1,4]
-n = len(nums)
-i = 0
-while i + nums[i] < n:
-    m = max(nums[i:i+nums[i]+1])
-    z = nums[i:i+nums[i]+1:-1]
-    i += z.index(m)
-    print(z)
+s = ""
+print(len(s))
