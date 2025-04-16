e077a4c8de415fab20cad90fb3481b9633fa19f7
tungisdab <khanhdzno1st01061102@gmail.com>
2025-04-16 21:45:16 +0700

    16.04.2025

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,2 +1,19 @@
-s = ""
-print(len(s))
+arr = [32,66,73,15,3,70,53]
+pieces = [[66,73],[15],[3],[32],[70],[53]]
+x = ''
+for i in arr:
+    x = x + str(i)
+print(x)
+for i in pieces:
+    m = ''
+    for j in i:
+        m = m + str(j)
+    
+    if m not in x:
+        # return False
+        print('False')
+    else:
+        x = x.replace(m, 'z', 1)
+    print(m, x)
+# return True if x.isalpha() else False
+print('True') if x.isalpha() else print('False')
